bba06a5c9515ab6cbaab53f066d5ab9556cace57
Riccardo-cpt <riccardo.9giuliani.6@gmail.com>
2026-06-15 15:05:30 +0200

    feat: add Supabase config constants to AdminConfig

--- lib/config/admin_config.dart
+++ lib/config/admin_config.dart
@@ -1,3 +1,11 @@
 class AdminConfig {
   static const String password = 'admin123';
+  static const String supabaseUrl = 'https://snsvamcecgizhecvtpwk.supabase.co';
+  static const String supabaseAnonKey =
+      'F6092D47-06DF-4FEF-86CB-10312D4B87CC';
+  static const String supabaseServiceRoleKey =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNuc3ZhbWNlY2dpemhlY3Z0cHdrIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc4MTUxMTg4MywiZXhwIjoyMDk3MDg3ODgzfQ.EVH2iQBi3Aox6rdWgs3B1jFmg0CZ2j829DgbAV3_b-I';
+  static const String supabaseRestUrl = '$supabaseUrl/rest/v1';
+  static const String supabaseStorageUrl = '$supabaseUrl/storage/v1';
+  static const String articoliBucket = 'articoli-images';
 }
