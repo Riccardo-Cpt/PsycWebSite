d2d346c523b658d67f45b55cdfaa7ec9fbf5d6c6
Riccardo-cpt <riccardo.9giuliani.6@gmail.com>
2026-06-15 15:07:03 +0200

    feat: add Articolo model with fromJson/toJson

---
+++ lib/models/articolo.dart
@@ -0,0 +1,32 @@
+class Articolo {
+  final int id;
+  final String titolo;
+  final String corpo;
+  final DateTime? pubblicatoAt;
+  final String? immagineUrl;
+
+  const Articolo({
+    required this.id,
+    required this.titolo,
+    required this.corpo,
+    this.pubblicatoAt,
+    this.immagineUrl,
+  });
+
+  factory Articolo.fromJson(Map<String, dynamic> json) => Articolo(
+        id: json['id'] as int,
+        titolo: json['titolo'] as String,
+        corpo: json['corpo'] as String,
+        pubblicatoAt: json['pubblicato_at'] != null
+            ? DateTime.parse(json['pubblicato_at'] as String)
+            : null,
+        immagineUrl: json['immagine_url'] as String?,
+      );
+
+  Map<String, dynamic> toJson() => {
+        'titolo': titolo,
+        'corpo': corpo,
+        'pubblicato_at': pubblicatoAt?.toIso8601String(),
+        'immagine_url': immagineUrl,
+      };
+}
