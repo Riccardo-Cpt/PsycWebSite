2441b803d1f98f122e755e642d121f40684eed82
Riccardo-cpt <riccardo.9giuliani.6@gmail.com>
2026-06-15 15:42:17 +0200

    fix: replace anon key placeholder with real Supabase JWT

--- lib/config/admin_config.dart
+++ lib/config/admin_config.dart
@@ -2,7 +2,7 @@ class AdminConfig {
   static const String password = 'admin123';
   static const String supabaseUrl = 'https://snsvamcecgizhecvtpwk.supabase.co';
   static const String supabaseAnonKey =
-      'F6092D47-06DF-4FEF-86CB-10312D4B87CC';
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNuc3ZhbWNlY2dpemhlY3Z0cHdrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE1MTE4ODMsImV4cCI6MjA5NzA4Nzg4M30.6Cj5ZmzJOojCt6lebpbsZHBCwNZZceG9YD7pCPtdHvQ';
   static const String supabaseServiceRoleKey =
       'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNuc3ZhbWNlY2dpemhlY3Z0cHdrIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc4MTUxMTg4MywiZXhwIjoyMDk3MDg3ODgzfQ.EVH2iQBi3Aox6rdWgs3B1jFmg0CZ2j829DgbAV3_b-I';
   static const String supabaseRestUrl = '$supabaseUrl/rest/v1';
